9f10d5ee98c3a46402eaa0041a85b57a774277c3
xiaoye-hua <huag@kth.se>
2021-10-26 06:35:15 +0800

    move mooc material to WPS

--- TODO/MOOC/Done_Recommender_system_evaluation_and_metrics/assignment/assgiment2/cfg/fallback.groovy
+++
@@ -1,8 +0,0 @@
-import org.lenskit.api.ItemScorer
-import org.lenskit.baseline.BaselineScorer
-import org.lenskit.bias.BiasItemScorer
-import org.lenskit.bias.BiasModel
-import org.lenskit.bias.UserItemBiasModel
-
-bind (BaselineScorer, ItemScorer) to BiasItemScorer
-bind BiasModel to UserItemBiasModel
